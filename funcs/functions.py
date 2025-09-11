9741118a24e7cdb26e7298de1273e47c8a389c64
Nikita Raylean <nekithis@gmail.com>
2025-09-11 21:27:50 +0300

    Added check existence of acc

--- funcs/functions.py
+++ funcs/functions.py
@@ -1,11 +1,19 @@
 from accounts import user_temp as acc
+import os
 
 
 def create_acc():
     with open(r'accounts\user.txt', 'a') as file:
         for i in acc.user:
-            user_inp = input(f'Enter your {i.lower()}')
+            user_inp = input(f'Enter your {i.lower()}: ')
             file.write(f"{i}: {user_inp}\n")
+            
+            
+def chk_acc_exist(login):
+    return  True if os.path.isfile(f'accounts\\{login}.txt') else False
+
+
+    
             
             
 
